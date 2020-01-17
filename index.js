60798f479012b1244b8d535ead850d66a91dbf34
Rachel Carranza <rachel.v.carranza@gmail.com>
2020-01-17 21:18:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ function happyHolidays (){
   return "Happy holidays!";
 }
 
-function happyHolidaysTo (name){
-  return `Happy holidays, ${name}!`
+function happyHolidayTo (name){
+  return `Happy holidays, ${name}!`;
 }
 
-function happyHolidaysTo (holiday, name){
-  return `Happy ${holiday}, ${name}!`
+function happyHolidayTo (holiday, name){
+  return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown (days, holiday){
-  return `It\'s ${days} days until ${holiday}!`
+  return `It\'s ${days} days until ${holiday}!`;
 }
